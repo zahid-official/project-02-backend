df9f44440bfbc04c6e9c69da7d3cbf1bf3d75544
zahid-official <zahid.official8@gmail.com>
2025-11-03 22:00:04 +0600

    Organizing prisma schema into multiple files

--- prisma/schema/migrations/20251103155928_init_users/migration.sql
+++ prisma/schema/migrations/20251103155928_init_users/migration.sql
@@ -1,9 +1,3 @@
-/*
-  Warnings:
-
-  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
-
-*/
 -- CreateEnum
 CREATE TYPE "UserRole" AS ENUM ('ADMIN', 'DOCTOR', 'PATIENT');
 
@@ -12,9 +6,6 @@ CREATE TYPE "UserStatus" AS ENUM ('ACTIVE', 'INACTIVE', 'DELETED');
 
 -- CreateEnum
 CREATE TYPE "Gender" AS ENUM ('MALE', 'FEMALE');
-
--- DropTable
-DROP TABLE "public"."User";
 
 -- CreateTable
 CREATE TABLE "users" (
