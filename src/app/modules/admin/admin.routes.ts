dacd8bc8ff8231a50d8c927fe436b2545f5f0704
zahid-official <zahid.official8@gmail.com>
2025-11-07 18:27:02 +0600

    Create token verify middleware to prevent unauthorized access

--- src/app/modules/admin/admin.routes.ts
+++ src/app/modules/admin/admin.routes.ts
@@ -3,6 +3,8 @@ import multerUploader from "../../config/multer";
 import { validateZodSchema } from "../../middlewares/validateZodSchema";
 import { createAdminZodSchema } from "./admin.validation";
 import AdminController from "./admin.controller";
+import validateToken from "../../middlewares/validateToken";
+import { UserRole } from "@prisma/client";
 
 // Initialize router
 const router = Router();
@@ -10,6 +12,7 @@ const router = Router();
 // Post routes
 router.post(
   "/create",
+  validateToken(UserRole.ADMIN),
   multerUploader.single("file"),
   validateZodSchema(createAdminZodSchema),
   AdminController.createAdmin
--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import UserController from "./user.controller";
+import validateToken from "../../middlewares/validateToken";
+import { UserRole } from "@prisma/client";
 
 // Initialize router
 const router = Router();
 
 // Get routes
-router.get("/", UserController.getAllUsers);
+router.get("/", validateToken(UserRole.ADMIN), UserController.getAllUsers);
 
 // Export user routes
 const UserRoutes = router;
